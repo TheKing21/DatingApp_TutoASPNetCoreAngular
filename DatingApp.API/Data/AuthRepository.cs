487346bb724374c5773067a1c8e1eba1bd110adf
TheKing21 <mathieu.tremblay21@gmail.com>
2018-11-11 21:33:04 -0500

    End of section 3 (User and Auth)

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> IsUserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.Username == username);
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }        
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash;
+            byte[] passwordSalt;
+
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            if (password == null || passwordHash == null)
+                return false;
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                if (computedHash.Length != passwordHash.Length)
+                    return false;
+
+                for (int cpt = 0; cpt < computedHash.Length; cpt++)
+                {
+                    if (computedHash[cpt] != passwordHash[cpt])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DatingApp.API/Dtos/dtoUserLogin.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Dtos
+{
+    public class dtoUserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
